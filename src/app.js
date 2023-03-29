14c1af0f2aa42c1dc3acddb0ebe6bc7deec6f4d5
Aleksandr Platcev <a.platcev@yandex.ru>
2023-03-29 14:25:32 +0300

    init and add solutions

--- src/app.js
+++ src/app.js
@@ -1,44 +1,155 @@
-/* eslint-disable func-names */
-import goblin from './img/goblin.png';
+/* eslint-disable no-use-before-define */
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-restricted-syntax */
+const dataInput = [
+  {
+    id: 26,
+    title: 'Побег из Шоушенка',
+    imdb: 9.30,
+    year: 1994,
+  },
+  {
+    id: 25,
+    title: 'Крёстный отец',
+    imdb: 9.20,
+    year: 1972,
+  },
+  {
+    id: 27,
+    title: 'Крёстный отец 2',
+    imdb: 9.00,
+    year: 1974,
+  },
+  {
+    id: 1047,
+    title: 'Тёмный рыцарь',
+    imdb: 9.00,
+    year: 2008,
+  },
+  {
+    id: 223,
+    title: 'Криминальное чтиво',
+    imdb: 8.90,
+    year: 1994,
+  },
+];
 
-function getRandomInt() {
-  return Math.floor(Math.random() * 16);
+function createDOM(data) {
+  for (const obj of data) {
+    // eslint-disable-next-line object-curly-newline
+    const { id, title, imdb, year } = obj;
+
+    const tr = document.createElement('tr');
+    tr.dataset.id = id;
+    tr.dataset.title = title;
+    tr.dataset.year = year;
+    tr.dataset.imdb = imdb;
+
+    createElements(tr);
+
+    const tbody = document.querySelector('tbody');
+    tbody.append(tr);
+  }
 }
+function createElements(parent) {
+  const items = [
+    parent.dataset.id,
+    parent.dataset.title,
+    parent.dataset.year,
+    parent.dataset.imdb,
+  ];
 
-function createElement() {
-  const img = document.createElement('img');
-  img.className = 'img';
-  img.src = goblin;
+  let count = 0;
+  while (count < items.length) {
+    const td = document.createElement('td');
 
-  return img;
-}
+    if (items[count] === parent.dataset.year) {
+      td.textContent = `(${items[count]})`;
+      parent.append(td);
+    } else if (items[count] === parent.dataset.imdb) {
+      td.textContent = `IMDB: ${items[count]}`;
+      parent.append(td);
+    } else {
+      td.textContent = items[count];
+      parent.append(td);
+    }
 
-function changeSection() {
-  const currentPosition = window.localStorage.getItem('position');
-  let nextPosition = getRandomInt();
-
-  while (nextPosition === currentPosition) {
-    nextPosition = getRandomInt();
+    count += 1;
   }
 
-  const sections = document.querySelectorAll('.section');
-  const img = createElement();
-
-  sections[currentPosition].firstChild.remove();
-  sections[nextPosition].append(img);
-
-  window.localStorage.setItem('position', nextPosition);
+  return parent;
 }
 
-function start() {
-  const startPosition = getRandomInt();
-  const img = createElement();
-  const sections = document.querySelectorAll('.section');
+function sortTable({ rule, attr }) {
+  const tbody = document.querySelector('tbody');
+  const trChildren = tbody.querySelectorAll('tr');
+  const trArray = [...trChildren];
 
-  window.localStorage.setItem('position', startPosition);
+  tbody.replaceChildren();
 
-  return sections[startPosition].append(img);
+  if (rule === 'min') {
+    trArray.sort((a, b) => a.dataset[attr] - b.dataset[attr]);
+  } else if (rule === 'max') {
+    trArray.sort((a, b) => b.dataset[attr] - a.dataset[attr]);
+  }
+
+  trArray.forEach((parent) => {
+    parent.replaceChildren();
+    createElements(parent, parent.dataset);
+    tbody.append(parent);
+  });
 }
 
-start();
-setInterval(changeSection, 1500);
+function sortRule() {
+  const rules = {
+    1: {
+      rule: 'min',
+      attr: 'id',
+    },
+    2: {
+      rule: 'max',
+      attr: 'id',
+    },
+    3: {
+      rule: 'min',
+      attr: 'title',
+    },
+    4: {
+      rule: 'max',
+      attr: 'title',
+    },
+    5: {
+      rule: 'min',
+      attr: 'year',
+    },
+    6: {
+      rule: 'max',
+      attr: 'year',
+    },
+    7: {
+      rule: 'min',
+      attr: 'imdb',
+    },
+    8: {
+      rule: 'max',
+      attr: 'imdb',
+    },
+  };
+
+  const rule = window.localStorage.getItem('rule');
+
+  if (!rule) {
+    window.localStorage.setItem('rule', 2);
+    return sortTable(rules[1]);
+  }
+  if (rule === 8) {
+    window.localStorage.setItem('rule', 1);
+  } else {
+    window.localStorage.setItem('rule', +rule + 1);
+  }
+
+  return sortTable(rules[rule]);
+}
+
+createDOM(dataInput);
+setInterval(sortRule, 5000);
